becd093c773eb6c825fbf459021b22a7d73eee06
monibu1548 <monibu1548@gmail.com>
2022-07-11 22:09:05 +0900

    Interaction URL 추가

--- functions/src/lib/contants.ts
+++ functions/src/lib/contants.ts
@@ -3,3 +3,9 @@ export enum ServicePath {
   Hi = "/hi",
   Remind = '/remind'
 }
+
+// 슬래시 커맨드에서 필요한 end point 2개
+export enum EndPoint {
+  Request = "/",
+  Interaction = "/interaction"
+}
--- functions/src/service/hi/router.ts
+++ functions/src/service/hi/router.ts
@@ -1,11 +1,21 @@
 import * as express from "express";
 import { CommandResponse, ResponseType } from "../../interface/commandReponse";
 import { CommandRequest } from "../../interface/commandRequest";
+import { EndPoint } from "../../lib/contants";
 
 const router = express.Router();
 
 // 슬래시 커맨드 실행 시 호출되는 Router
-router.post('/', async (req: express.Request, res: express.Response) => {
+router.post(EndPoint.Request, async (req: express.Request, res: express.Response) => {
+  const request = req.body as CommandRequest
+
+  res.status(200).json({
+    text: request.text,
+    responseType: ResponseType.InChannel
+  } as CommandResponse)
+});
+
+router.post(EndPoint.Interaction, async (req: express.Request, res: express.Response) => {
   const request = req.body as CommandRequest
 
   res.status(200).json({
