b0b8e53b03fb66f0cdb5a9e1442cf46595aab470
monibu1548 <monibu1548@gmail.com>
2022-07-11 22:41:48 +0900

    샘플 추가

--- functions/src/interface/commandReponse.ts
+++ functions/src/interface/commandReponse.ts
@@ -3,9 +3,27 @@ export enum ResponseType {
   InChannel = 'inChannel'
 }
 
+export interface AttachmentButtons {
+  callbackId: string,
+  title: string,
+  actions: [AttachmentButtonAction] | null
+}
+
+export enum AttachmentButtonActionType {
+  Button = 'button'
+}
+
+export interface AttachmentButtonAction {
+  name: string,
+  type: AttachmentButtonActionType,
+  text: string,
+  value: number
+}
+
 export interface CommandResponse {
   text: string, // 메시지 내용
   responseType: ResponseType, // Ephemeral (나에게만 보이는 메시지), InChannel (채널에 공유되는 메시지)
   replaceOriginal: boolean, // 기존에 보낸 메시지를 업데이트 (알림 X)
-  deleteOriginal: boolean // 기존 메시지를 삭제하고 새로 전송 (알림 O)
+  deleteOriginal: boolean, // 기존 메시지를 삭제하고 새로 전송 (알림 O)
+  attachments: [AttachmentButtons] | null
 }
--- functions/src/service/hi/router.ts
+++ functions/src/service/hi/router.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { CommandResponse, ResponseType } from "../../interface/commandReponse";
+import { AttachmentButtonActionType, CommandResponse, ResponseType } from "../../interface/commandReponse";
 import { CommandRequest } from "../../interface/commandRequest";
 import { EndPoint } from "../../lib/contants";
 
@@ -9,10 +9,61 @@ const router = express.Router();
 router.post(EndPoint.Request, async (req: express.Request, res: express.Response) => {
   const request = req.body as CommandRequest
 
-  res.status(200).json({
-    text: request.text,
-    responseType: ResponseType.InChannel
-  } as CommandResponse)
+  var response: CommandResponse
+
+  switch (request.text) {
+    case 'A':
+      response = {
+        text: "a-menu",
+        deleteOriginal: false,
+        responseType: ResponseType.InChannel,
+        replaceOriginal: false,
+        attachments: null
+      }
+      break;
+    case 'b-menu':
+      response = {
+        text: "b-menu",
+        deleteOriginal: false,
+        responseType: ResponseType.InChannel,
+        replaceOriginal: false,
+        attachments: null
+      }
+      break;
+    case 'c-menu':
+      response = {
+        text: "c-menu",
+        deleteOriginal: false,
+        responseType: ResponseType.Ephemeral,
+        replaceOriginal: false,
+        attachments: [
+          {
+            callbackId: 'f',
+            title: '버튼 타이틀',
+            actions: [
+              {
+                name: '이름1',
+                type: AttachmentButtonActionType.Button,
+                text: '텍스트1',
+                value: 0
+              },
+            ]
+          }
+        ]
+      }
+      break;
+    default:
+      response = {
+        text: "명령어를 찾을 수 없습니다.",
+        deleteOriginal: false,
+        responseType: ResponseType.InChannel,
+        replaceOriginal: false,
+        attachments: null
+      }
+      break;
+  }
+
+  res.status(200).json(response)
 });
 
 // 사용자의 상호작용시 호출되는 Router
